92f18d261e934966f72817ef5d9eeed0fac34360
Niall O'Higgins <niallo@unworkable.org>
2012-06-24 14:42:06 -0700

    seems to work.

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 node-engineer
 =============
 
-Parse package.json, inspect engine property and ensure command runs with right node.js version
+Parse package.json, inspect engine property and ensure command runs with right node.js version.
+
+Engineer makes it trivial to run a command in an environment where `node` and `npm` match those
+specified by a particular package.json's `engine` field.
+
+This makes it a no-brainer to run an arbitrary package under whatever version of node it needs. Engineer uses `nave` and `semver` to do the heavy lifting ot installing and checking for valid node.js versions.
+
+Installation
+============
+```
+npm install engineer
+```
+
+Usage
+=====
+```
+execute command with node version acceptable to engine package.json property
+usage: node ./main.js
+
+Options:
+  -c  Shell command         [required]
+  -f  Path to package.json  [required]
+
+Missing required arguments: c, f
+```
+
+Demo
+====
+
+```
+$ engineer -f package.json -c 'npm ls'
+local versions: []
+Remote version 0.6.19 satisfies range >=0.6.0 <0.7.0
+running cmd: ["./node_modules/nave/nave.sh","use","0.6.19","npm","ls"]
+Already installed: 0.6.19
+
+using 0.6.19
+
+node-engineer@0.0.1 /Users/nialljohiggins/projects/node-engineer
+├── chai@1.0.4 
+├─┬ mocha@1.2.0 
+│ ├── commander@0.5.2 
+│ ├── debug@0.7.0 
+│ ├── diff@1.0.2 
+│ ├── growl@1.5.1 
+│ └─┬ jade@0.20.3 
+│   └── mkdirp@0.3.3 
+├── nave@0.2.13 
+├─┬ optimist@0.3.4 
+│ └── wordwrap@0.0.2 
+└── semver@1.0.14 
+```
---
+++ main.js
@@ -0,0 +1,149 @@
+var argv = require('optimist')
+    .usage("execute command with node version acceptable to engine package.json property\nusage: $0")
+    .demand('c')
+    .describe('c', 'Shell command')
+    .demand('f')
+    .describe('f', 'Path to package.json')
+    .argv
+    exec = require('child_process').exec,
+    fs = require('fs'),
+    semver = require('semver'),
+    spawn = require('child_process').spawn;
+
+var nave_path = "./node_modules/nave/nave.sh";
+
+function readPackage(path) {
+  try {
+    return(fs.readFileSync(path));
+  } catch(e) {
+    console.log(e.toString());
+    process.exit(1);
+  }
+}
+
+function parsePackage(contents) {
+  try {
+    return(JSON.parse(contents));
+  } catch(e) {
+    console.log("Error: Could not parse package.json:", e);
+    process.exit(1);
+  }
+}
+
+function getEngine(pkgData) {
+  var engine;
+  if (data.engines && data.engines.node) {
+    engine = data.engines.node;
+  } else {
+    engine = "*";
+  }
+  return engine;
+}
+// Look in ~/.nave/installed to list currently installed Node.JS versions
+// @param cb function(err, [versions])
+function getLocalNodeVers(cb) {
+  var nave_path =  process.env.HOME + "/.nave/installed/";
+
+  fs.readdir(nave_path, cb);
+}
+
+// Use `nave ls-remote` to build a list of available node.js versions
+// @param cb function([versions])
+function getRemoteNodeVers(cb) {
+  var cmd = nave_path + " ls-remote";
+
+  exec(cmd, function(err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    // Filter out 'remote:'
+    stdout = stdout.replace('remote:', '');
+    var versions = stdout.split(/\s+/);
+    var res = [];
+    // Filter empty strings
+    for (var i=0; i < versions.length; i++) {
+      if (versions[i].length > 0) {
+        res.push(versions[i]);
+      }
+    }
+
+    cb(res);
+  });
+}
+
+function run(cmd, cb, hide_dl) {
+  cmd = cmd.split(/\s+/);
+
+  console.log("running cmd: %j", cmd);
+  var child = spawn(cmd[0], cmd.slice(1));
+  var done = false;
+  child.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
+  child.stderr.on('data', function(data) {
+    var d = data.toString()
+    // Try to filter out most of the curl download
+    if (hide_dl == true) {
+      if (!done && d.indexOf('100.0%') != -1) {
+        console.log("download complete");
+        done = true;
+      }
+      if (done) {
+        console.log(d);
+      }
+    } else {
+      console.log(d);
+    }
+  });
+  child.on('exit', function(code) {
+    if (code != 0) {
+      console.log("Error executing command %s: %s", cmd, err);
+      process.exit(1);
+    }
+    cb();
+  });
+}
+
+var json = readPackage(argv.f);
+var data = parsePackage(json);
+var engine = getEngine(data);
+
+getLocalNodeVers(function(err, versions) {
+  if (err) {
+    versions = [];
+  }
+  console.log("local versions: %j", versions);
+  // Check whether an already-installed node version satisfies the engine
+  var found = false;
+  for (var i=0; i<versions.length; i++) {
+    if (semver.satisfies(versions[i], engine)) {
+      found = versions[i];
+      break;
+    }
+  }
+  var cmd = nave_path + " use " + found + " " + argv.c;
+  if (found) {
+    console.log("Local version %s satisfies range %s", found, engine);
+    run(cmd, function() {
+        process.exit(0);
+    }, true);
+  } else {
+    // No locally-installed version satisfies, find the latest remote version 
+    // install that, then run the command with that version.
+    getRemoteNodeVers(function(versions) {
+      var remote_version = semver.maxSatisfying(versions, engine);
+      if (!remote_version) {
+        console.log("Error: No remote version can satisfy range %s!", engine);
+        process.exit(1);
+      }
+      console.log("Remote version %s satisfies range %s", remote_version, engine);
+      var cmd = nave_path + " use " + remote_version + " " + argv.c;
+      run(cmd, function() {
+        process.exit(0);
+      }, true);
+    });
+  }
+
+});
+
